3269a26988a0c481f97e13b5cae55faa5365bd03
Bryan Bishop <bryan.bishop@pearson.com>
2016-08-26 21:53:23 -0500

    Fix the logging by listing during backup creation

--- borg.sh
+++ borg.sh
@@ -4,13 +4,13 @@ source $HOME/src/borg/env.sh
 ITEM=`uuidgen | tail -c 10`
 mkdir -p $BORG_MANIFESTS_HOME
 
-osascript -e "tell application \"Box Sync\" to activate"
-sleep 30
+# osascript -e "tell application \"Box Sync\" to activate"
+# sleep 30
 
-nice /usr/local/bin/borg create $OPTIONS $VAULT::$ITEM $HOME || borg_notify "Backup unsuccessful!"
-$HOME/src/borg/prune.sh
+/usr/local/bin/borg create $OPTIONS $VAULT::$ITEM $HOME &> $BORG_MANIFESTS_HOME/$ITEM.txt || borg_notify "Backup unsuccessful!"
+# $HOME/src/borg/prune.sh
 
-/usr/local/bin/borg list $VAULT::$ITEM > $BORG_MANIFESTS_HOME/$ITEM.txt
-nice xz -9 $BORG_MANIFESTS_HOME/$ITEM.txt
-sleep 90
-osascript -e "tell application \"Box Sync\" to quit"
+# /usr/local/bin/borg list $VAULT::$ITEM > $BORG_MANIFESTS_HOME/$ITEM.txt
+nice /usr/local/bin/xz -9 $BORG_MANIFESTS_HOME/$ITEM.txt
+# sleep 90
+# osascript -e "tell application \"Box Sync\" to quit"
--- env.sh
+++ env.sh
@@ -7,4 +7,4 @@ VAULT=/Volumes/Bytor/box/borg
 BORG_HOME=$HOME/src/borg
 BORG_MANIFESTS_HOME=$BORG_HOME/manifests
 EXCLUDE=$BORG_HOME/exclude
-OPTIONS="--exclude-from $EXCLUDE --one-file-system --verbose --stats --compression zlib"
+OPTIONS="--exclude-from $EXCLUDE --one-file-system --verbose --stats --compression zlib --list"
