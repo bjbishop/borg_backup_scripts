211495d8b13f8ce61c57cac392e9e2797dc45c11
Bryan Bishop <bryan.bishop@pearson.com>
2016-11-21 15:37:00 -0600

    Update pre-commit, add an exclude file

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@ VAULT=/Volumes/Bytor/box/borg
 BORG_HOME=$HOME/src/borg
 BORG_MANIFESTS_HOME=$BORG_HOME/manifests
 EXCLUDE=$BORG_HOME/exclude
-OPTIONS="--exclude-from $EXCLUDE --one-file-system --verbose --stats --compression zlib --list --lock-wait 60"
+OPTIONS="--exclude-from $EXCLUDE --one-file-system --verbose --stats --compression zlib --list --lock-wait 60 --exclude-if-present .borgexclude"
