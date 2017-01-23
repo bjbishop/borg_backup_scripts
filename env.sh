f927c39fd2235abbc09e95f7b553091a97272837
Bryan Bishop <bryan.bishop@pearson.com>
2017-01-23 13:26:22 -0600

    Use lzma compression and default some of the env variables

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
-VAULT=/Users/bishbr/box/borg-encrypted
+VAULT=${VAULT:-/Users/bishbr/box/borg-encrypted}
 BORG_HOME=$HOME/src/borg
 BORG_MANIFESTS_HOME=$BORG_HOME/manifests
 EXCLUDE=$BORG_HOME/exclude
-OPTIONS="--exclude-from $EXCLUDE --one-file-system --verbose --stats --compression zlib --list --lock-wait 60 --exclude-if-present .borgexclude"
+OPTIONS="--exclude-from $EXCLUDE --one-file-system --verbose --stats --compression lzma --list --lock-wait 60 --exclude-if-present .borgexclude"
 
 function borg_notify() {
     command -v terminal-notifier > /dev/null || brew install terminal-notifier
