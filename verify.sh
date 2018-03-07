96f1414dc9d5a4bd093b78f334468e660f1a4c0a
Bryan Bishop <bryan.bishop@pearson.com>
2018-03-07 10:17:29 -0600

    Remove older files from before borgmatic

--- verify.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/bash
-
-source $HOME/src/borg/env.sh
-OPTIONS="--repair --repository-only --lock-wait 30 --verbose"
-
-yes YES | $HOME/src/borg/borg check $OPTIONS $VAULT || borg_notify "Verify unsuccessful!"
-
