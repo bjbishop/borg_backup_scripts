2948994c18e0eddb6c08827bcc3616d4cbbad3f3
Bryan Bishop <bryan.bishop@pearson.com>
2019-05-22 10:30:25 -0500

    Remove old junk :zap:

--- borgmatic.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/bash
-
-export PATH=/usr/local/bin:$PATH
-export BORG_REPO=~/box/borgmatic
-export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
-export BORG_PASSCOMMAND="security -q find-generic-password -l 'borgmatic version1' -w"
-nice /usr/local/bin/borgmatic -c ~/src/borg/borgmatic-box.yaml -v 1
